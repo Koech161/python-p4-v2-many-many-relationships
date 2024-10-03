06029b340c1063ba74e06f1702cdf8d1ac3500d4
Abel Koech <koechabel161@gmail.com>
2024-10-03 06:36:23 +0300

    many-tom-many

--- server/models.py
+++ server/models.py
@@ -9,6 +9,14 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+employee_meetings = db.Table(
+    'employee_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey(
+        'employees.id'), primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey(
+        'meetings.id'), primary_key=True)
+)
 
 
 class Employee(db.Model):
@@ -17,6 +25,14 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+
+    meetings = db.relationship('Meeting', secondary=employee_meetings, back_populates='employees')
+    assignments = db.relationship(
+        'Assignment', back_populates='employee', cascade='all, delete-orphan')
+
+    # Association proxy to get projects for this employee through assignments
+    projects = association_proxy('assignments', 'project',
+                                 creator=lambda project_obj: Assignment(project=project_obj))
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
@@ -29,7 +45,8 @@ class Meeting(db.Model):
     topic = db.Column(db.String)
     scheduled_time = db.Column(db.DateTime)
     location = db.Column(db.String)
-
+    employees = db.relationship(
+        'Employee', secondary=employee_meetings, back_populates='meetings')
     def __repr__(self):
         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
 
@@ -40,6 +57,13 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     budget = db.Column(db.Integer)
+
+    assignments = db.relationship(
+        'Assignment', back_populates='project', cascade='all, delete-orphan')
+
+    # Association proxy to get employees for this project through assignments
+    employees = association_proxy('assignments', 'employee',
+                                  creator=lambda employee_obj: Assignment(employee=employee_obj))
 
     def __repr__(self):
         return f'<Review {self.id}, {self.title}, {self.budget}>'
